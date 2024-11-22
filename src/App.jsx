16bf13e94fef7d037b8c4d2613c2f4d75b383fa7
damonryon <damon.ryon@gmail.com>
2024-11-22 15:16:41 -0700

    initial form complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <UserInfoForm/>
+    </div>
+  )
+}
+
+const UserInfoForm = () => {
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  return (
+  <form onSubmit={(e) => {
+    e.preventDefault()
+    alert(`Hello ${firstName} ${lastName}`)
+    setFirstName("")
+    setLastName("")
+  }}>
+
+    <div>
+      <label>First Name: </label>
+      <input type="text"
+        value={firstName}
+        onChange={(e) => {
+          setFirstName(e.target.value)
+        }}
+        />
+    </div>
+    <div>
+      <label>Last Name: </label>
+      <input type="text"
+        value={lastName}
+        onChange={(e) => {
+          setLastName(e.target.value)
+        }}
+      />
+    </div>
+    <input type="submit" />
+  </form>
   )
 }
 
