17bdae3bbaa8fedf1d0b404ace27690fa224fae4
damonryon <damon.ryon@gmail.com>
2024-11-25 12:39:28 -0700

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
-
   return (
     <div className="App">
       <UserInfoForm/>
@@ -15,33 +13,32 @@ const UserInfoForm = () => {
   const [firstName, setFirstName] = useState("")
   const [lastName, setLastName] = useState("")
   return (
-  <form onSubmit={(e) => {
-    e.preventDefault()
-    alert(`Hello ${firstName} ${lastName}`)
-    setFirstName("")
-    setLastName("")
-  }}>
-
-    <div>
-      <label>First Name: </label>
-      <input type="text"
-        value={firstName}
-        onChange={(e) => {
-          setFirstName(e.target.value)
-        }}
+    <form onSubmit={(e) => {
+      e.preventDefault()
+      alert(`Hello ${firstName} ${lastName}`)
+      setFirstName('')
+      setLastName('')
+     }}>
+      <div>
+        <label>First Name: </label>
+        <input type="text"
+          value={firstName}
+          onChange={(e) => {
+            setFirstName(e.target.value)
+          }}
         />
-    </div>
-    <div>
-      <label>Last Name: </label>
-      <input type="text"
-        value={lastName}
-        onChange={(e) => {
-          setLastName(e.target.value)
-        }}
-      />
-    </div>
-    <input type="submit" />
-  </form>
+      </div>
+      <div>
+        <label htmlFor="">Last Name: </label>
+        <input type="text"
+          value={lastName}
+          onChange={(e) => {
+            setLastName(e.target.value)
+          }}
+        />
+      </div>
+      <input type="submit" />
+     </form>
   )
 }
 
