f5e03d683b60bf21ca87572de1a7b8a27cb500d9
damonryon <damon.ryon@gmail.com>
2024-12-03 14:32:24 -0700

    refactor form

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,68 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+export function UserInfo({userInformation}){
+  const firstName = userInformation?.firstName
+  const lastName = userInformation?.lastName
+  return <div>
+    <div>First Name: {firstName}</div>
+    <div>Last Name: {lastName}</div>
+  </div>
+}
 
+function App() {
+  const [userInformation, setUserInformation] = useState(null)
   return (
     <div className="App">
-      <UserInfoForm />
+      <UserInfo userInformation={userInformation} />
+      <UserInfoForm handleUserInformation={(userInformation) => {
+        setUserInformation(userInformation)
+      }}/>
     </div>
   )
 }
 
 
-const UserInfoForm = () => {
-  const [firstName, setFirstName] = useState("")
-  const [lastName, setLastName] = useState("")
+const UserInfoForm = ({handleUserInformation}) => {
+  const [firstNameInput, setFirstNameInput] = useState("")
+  const [lastNameInput, setLastNameInput] = useState("")
   return (
     <form onSubmit={(e) => {
       e.preventDefault()
-      alert(`Hello ${firstName} ${lastName}`)
-      setFirstName('')
-      setLastName('')
+      handleUserInformation({
+        firstName: firstNameInput,
+        lastName: lastNameInput
+      })
+      setFirstNameInput('')
+      setLastNameInput('')
     }}>
-      <div>
-        <label>First Name: </label>
-        <input type="text"
-          value={firstName}
-          onChange={(e) => {
-            setFirstName(e.target.value)
-          }}
-        />
-      </div>
-      <div>
-        <label>Last Name: </label>
-        <input type="text"
-          value={lastName}
-          onChange={(e) => {
-            setLastName(e.target.value)
-          }}
-        />
-      </div>
+      <TextInput
+        label={"First Name"}
+        onChange={(e) => {
+          setFirstNameInput(e.target.value)
+        }}
+        value={firstNameInput}/>
+      <TextInput
+        label={"Last Name"}
+        onChange={(e) => {
+          setLastNameInput(e.target.value)
+        }}
+        value={lastNameInput}/>
       <input type="submit" />
     </form>
+  )
+}
+
+
+const TextInput = ({ value, label, onChange }) => {
+  return (
+    <div>
+      <label>{label}: </label>
+      <input
+        type="text"
+        value={value}
+        onChange={onChange} />
+    </div>
   )
 }
 
